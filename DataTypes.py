2ef3c9c7aa19aa85d45188076f39a0a94ea09a0b
ChandraGolla <chandra704.golla@gmail.com>
2017-12-12 17:05:12 +0530

    My Python projects

---
+++ DataTypes.py
@@ -0,0 +1,34 @@
+#List
+list = [1,5,'Python']
+print(list)
+print(list[2])
+print(len(list))
+list[0]='One'
+print(list[0])
+
+#Dictionaries
+subject={'S':'Physics', 2:'Number', 'Fruit':'Apple'}
+print(subject)
+print(subject[2])
+print(subject.keys()); print(subject.values())
+
+#sets
+n1={1,2,3,4}
+print(n1)
+
+#Tuples
+mytuple=(1,5,'Python')
+print(mytuple[1])
+#We cannot update tuples like lists
+#Strings
+str1="Chandra Seker"
+print(str1)
+
+str2="""Chandra
+Sekhar"""
+print(str2)
+
+var1=var2=var3=100
+var4, var5, var6='Chandra',100, 'Golla'
+print(var1,var3)
+print(var4,var5,var6)
---
+++ DateTime.py
@@ -0,0 +1,8 @@
+import datetime
+import time
+import calendar
+print(time.time())
+#time.sleep(30)
+print(time.localtime(time.time()))
+#print(calendar.month(1982,7))
+print(calendar.calendar(1982,2,1,10))
